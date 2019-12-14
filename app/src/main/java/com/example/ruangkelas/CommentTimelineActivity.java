8ca6cc9fff1188ed1ca06984526b2ca95cdd9420
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2019-12-14 11:58:30 +0800

    Timeline Comment

---
+++ app/src/main/java/com/example/ruangkelas/CommentTimelineActivity.java
@@ -0,0 +1,13 @@
+package com.example.ruangkelas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommentTimelineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_timeline);
+    }
+}
